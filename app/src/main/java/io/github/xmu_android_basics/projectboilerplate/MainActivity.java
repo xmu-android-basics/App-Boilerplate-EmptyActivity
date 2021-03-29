1c8b8d72b1f4716c6dea9620640f4b712e7e1875
吴德文@厦门大学 <windwood@xmu.edu.cn>
2021-03-29 22:36:09 +0800

    新建 Empty Activity 项目 + Android Studio 版本为 4.1.3 (gradle-6.5) + 设定项目名称（ProjectBoilerplate，可以更改） + 设定 applicationId 为 io.github.xmu_android_basics.projectboilerplate + 选择最低运行环境为API 22 + 选择项目模板为 Empty Activity template + 使用 AndroidX

---
+++ app/src/main/java/io/github/xmu_android_basics/projectboilerplate/MainActivity.java
@@ -0,0 +1,14 @@
+package io.github.xmu_android_basics.projectboilerplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
