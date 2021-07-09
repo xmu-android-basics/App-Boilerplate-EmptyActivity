0093ee97cc62d00968dca197a5add6fc6c280849
Wind Wood <windwood@gmail.com>
2021-07-09 11:39:04 +0800

    add viewBinding feature, and update Activity's to using binding.

--- app/src/main/java/io/github/xmu_android_basics/projectboilerplate/MainActivity.java
+++ app/src/main/java/io/github/xmu_android_basics/projectboilerplate/MainActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.github.xmu_android_basics.projectboilerplate.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding _binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        _binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(_binding.getRoot());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        _binding = null;
     }
 }
